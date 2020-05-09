1c99c25106eb57e385b94a50b6b15ad186f34a99
neethu <msneethu2989@gmail.com>
2020-05-08 21:51:46 -0400

    Add routes

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { BookdetailComponent } from '../bookdetail/bookdetail.component';
+import { LibraryComponent} from '../library/library.component';
 
 export const routes: Routes=[
-  { path: 'bookdetail/:id', component: BookdetailComponent }
+  { path: 'bookdetail/:id', component: BookdetailComponent },
+
+  { path:'',component: LibraryComponent }
+
 ];
