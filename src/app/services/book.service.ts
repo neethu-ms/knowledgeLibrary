8cb31fdc0421bc637cab55473d7acebab41858bb
neethu <msneethu2989@gmail.com>
2020-05-29 17:47:09 -0400

    Add put method to update data in server

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Book } from '../shared/book';
-import { BOOKS } from '../shared/books';
-import { of, Observable} from 'rxjs';
+import { Observable} from 'rxjs';
 import { baseURL } from '../shared/baseurl';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +17,16 @@ export class BookService {
   getBook(id: number): Observable<Book> {
     return this.http.get<Book>(baseURL+'books/'+id);
 
+  }
+
+
+  putBook(book: Book): Observable<Book> {
+        const httpOptions  = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+    };
+
+    return this.http.put<Book>(baseURL+'books/'+book.id,book,httpOptions);
   }
 }
