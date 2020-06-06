b3b3ab3bd21a92b8024217aa6c8732edc2998f5b
neethu <msneethu2989@gmail.com>
2020-06-05 23:14:46 -0400

    Add condition to submit form only if it is valid'

--- src/app/bookdetail/bookdetail.component.ts
+++ src/app/bookdetail/bookdetail.component.ts
@@ -25,52 +25,57 @@ export class BookdetailComponent implements OnInit {
     private route: ActivatedRoute,
     private location: Location,
     private fb: FormBuilder
-
   ) {
     this.createForm();
   }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.params['id'];
-    this.bookService.getBook(id).subscribe((book) => (this.book = book), errmess => this.errMess = <any>errmess);
+    this.bookService.getBook(id).subscribe(
+      (book) => (this.book = book),
+      (errmess) => (this.errMess = <any>errmess)
+    );
   }
 
   goBack(): void {
     this.location.back();
   }
 
-
-  reserve():void{
-      this.book.status = false;
-      this.bookService.putBook(this.book).subscribe((book) => {
+  reserve(): void {
+    this.book.status = false;
+    this.bookService.putBook(this.book).subscribe(
+      (book) => {
         this.book = book;
-      }, errmess => this.errMess = <any>errmess);
-
+      },
+      (errmess) => (this.errMess = <any>errmess)
+    );
   }
   createForm() {
     this.feedbackForm = this.fb.group({
       author: ['', Validators.required],
       comment: ['', Validators.required],
-      rating:1
-
+      rating: 1,
     });
   }
 
   onSubmit() {
-    this.feedback = this.feedbackForm.value;
-    this.comment = this.feedbackForm.value;
-    this.comment.date = new Date().toISOString();
-    this.book.comments.push(this.comment);
-    this.bookService.putBook(this.book).subscribe((book) => {
-      this.book = book;
-    }, errmess => this.errMess = <any>errmess);
-     this.feedbackForm.reset({
-      author: '',
-      comment: '',
-      rating:1
-    });
-    this.feedbackFormDirective.resetForm();
+    if (this.feedbackForm.status === 'VALID') {
+      this.feedback = this.feedbackForm.value;
+      this.comment = this.feedbackForm.value;
+      this.comment.date = new Date().toISOString();
+      this.book.comments.push(this.comment);
+      this.bookService.putBook(this.book).subscribe(
+        (book) => {
+          this.book = book;
+        },
+        (errmess) => (this.errMess = <any>errmess)
+      );
+      this.feedbackForm.reset({
+        author: '',
+        comment: '',
+        rating: 1,
+      });
+      this.feedbackFormDirective.resetForm();
+    }
   }
-
-
 }
